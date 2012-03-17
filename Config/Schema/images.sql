2350657df2abc14d5f780d5276feef605d62b084
root <root@lamp1.(none)>
2012-03-17 18:21:17 -0500

    Initial commit

---
+++ Config/Schema/images.sql
@@ -0,0 +1,7 @@
+create table images (
+    id int unsigned not null auto_increment primary key,
+    name varchar(50) not null,
+    type varchar(25) not null,
+    image blob not null,
+    size int unsigned not null
+);
---
+++ Model/MysqlImageStorageAppModel.php
@@ -0,0 +1,6 @@
+<?php
+
+class MysqlImageStorageAppModel extends AppModel {
+
+}
+
